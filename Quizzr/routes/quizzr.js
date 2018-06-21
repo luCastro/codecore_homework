3f2fb0f67be2fca49ed5f13345a9faf5e7c4c021
Luciana Cunha <lucastro.marquise@gmail.com>
2018-06-21 16:33:36 -0700

    quizzs post  and get method

--- Quizzr/routes/quizzr.js
+++ Quizzr/routes/quizzr.js
@@ -2,10 +2,34 @@ const express = require('express');
 const router = express.Router();
 
 
-/* GET home page. */
+
 router.get('/', function(req, res, next) {
     res.render('quizzr');
   });
+
+
+const COOKIE_MAX_AGE = 1000 * 60 * 60 * 24;
+router.post('/', (req, res)=> {
+
+  const question1 = req.body.question1; 
+  
+  let result;
+   if(question1 === "option4"){
+      result = "It is correct";
+   }else{
+      result = "Try again";
+   }
+
+  res.cookie("ble", result, {maxAge: COOKIE_MAX_AGE});
+  
+  res.redirect("/quizzr/result");
+  
+});
+
+router.get('/result', (req, res)=>{
+  
+  res.render('result', {result: req.cookies.ble});
+});
   
   module.exports = router;
   
