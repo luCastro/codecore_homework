4bcc2fc7751dc85da2f85e8f9bb25e00a1c5ff7e
Luciana Cunha <lucastro.marquise@gmail.com>
2018-06-24 23:31:08 -0700

    Quizzr result logical/method POST

--- Quizzr/routes/quizzr.js
+++ Quizzr/routes/quizzr.js
@@ -10,19 +10,39 @@ router.get('/', function(req, res, next) {
 
 const COOKIE_MAX_AGE = 1000 * 60 * 60 * 24;
 router.post('/', (req, res)=> {
+  //res.json(req.body);
+  const obj = {
+    question1: req.body.question1,
+    question2: req.body.question2,
+    question3: req.body.question3,
+    question4: req.body.question4,
+    question5: req.body.question5,
+    question6: req.body.question6,
+    question7: req.body.question7,
+    question8: req.body.question8,
+    question9: req.body.question9,
+    question10: req.body.question10
+    
+  }
+  const resultArr = ["option2", "option1","option2","option1","option4", "option3", 
+  ["option3", "option4"],["option2", "option4"], ["option1", "option3"],
+  ["option1", "option2", "option3", "option4"]];
+  let result=0;
+  let i = 0;
+  let answer;
+  for (question in obj){ 
+      answer = obj[question];
 
-  const question1 = req.body.question1; 
-  // res.json(req.body);
-  let result;
-   if(question1 === "option4"){
-      result = "It is correct";
-   }else{
-      result = "Try again";
-   }
+      if(answer.toString() == resultArr[i].toString()){
+          result+=1;
+        }
+        i++;
+    }
+    result = result * 10;
 
-  res.cookie("ble", result, {maxAge: COOKIE_MAX_AGE});
+   res.cookie("ble", result, {maxAge: COOKIE_MAX_AGE});
   
-  res.redirect("/quizzr/result");
+   res.redirect("/quizzr/result");
   
 });
 
